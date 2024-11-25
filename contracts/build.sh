5d13542cc0d1f075cb83d554810b312ad8730568
ice-charon <antoniy.shumanov@leftclick.io>
2024-11-25 16:30:42 +0200

    Vesting wallet integration

--- contracts/build.sh
+++ contracts/build.sh
@@ -14,11 +14,17 @@ CRYPTO_SRC_PATH=${SRC_PATH}/crypto
 fift=${CRYPTO_EXEC_PATH}/fift
 func=${CRYPTO_EXEC_PATH}/func
 
-lite_client=${EXEC_PATH}/lite-client/lite-client
-config=${EXEC_PATH}/global.config.json
-
 inc=${CRYPTO_SRC_PATH}/fift/lib/:${CRYPTO_SRC_PATH}/smartcont/
+
+base_file=vesting-wallet # read private key from base_file.pk, save address in base_file.addr
+start_time=1732520581 # unix time start time of the vesting
+duration_time=10000 # duration of the vesting in seconds
+unlock_period=100 # period to unlock a portion of the vesting amount in seconds
+cliff_period=0 # delay first unlock period by specific time in seconds
+vesting_amount=10 # vesting amount in Grams
+sender_address=EQC4il794WSFzmB0EEJjcDtiE9__l-yb4nJzu7Kt_0hA8qDI # owner address can send portion of vesting amount to this address at any time
+owner_address=EQAkqfKFKSWsoC90q7mmOdIDJ2ScPlmip-BCLJHTtgLfFEsu # the owner of the vesting wallet, the address of private key in base-file.pk
 
 # multisig addr
 $func -o vesting-wallet.fif -SPA vesting_wallet.fc
-$fift -I ${inc} -s new-vesting-wallet.fif 0 268 vesting-wallet 1732286307 10000 100 0 10 EQC4il794WSFzmB0EEJjcDtiE9__l-yb4nJzu7Kt_0hA8qDI EQAkqfKFKSWsoC90q7mmOdIDJ2ScPlmip-BCLJHTtgLfFEsu
+$fift -I ${inc} -s new-vesting-wallet.fif 0 268 ${base_file} ${start_time} ${duration_time} ${unlock_period} ${cliff_period} ${vesting_amount} ${sender_address} ${owner_address}
--- contracts/send.sh
+++ contracts/send.sh
@@ -16,5 +16,18 @@ func=${CRYPTO_EXEC_PATH}/func
 
 inc=${CRYPTO_SRC_PATH}/fift/lib/:${CRYPTO_SRC_PATH}/smartcont/
 
+lite_client=${EXEC_PATH}/lite-client/lite-client
+config=global.config.json
+
+base_file=vesting-wallet # base-file.pk is used to sign, base-file.addr to add in external message
+dest_address=EQC4il794WSFzmB0EEJjcDtiE9__l-yb4nJzu7Kt_0hA8qDI
+amount=8 # amount to be send
+
+owner_address=$($fift -I ${inc} -s show-addr.fif ${base_file})
+seqno=$($lite_client --global-config ${config} --cmd "runmethod ${owner_address} seqno")
+seqno=${seqno#*result}
+seqno=${seqno##*[}
+seqno=$(echo ${seqno%]*} | xargs)
+
 # multisig addr
-$fift -I ${inc} -s send-vesting-wallet.fif vesting-wallet EQC4il794WSFzmB0EEJjcDtiE9__l-yb4nJzu7Kt_0hA8qDI 268 1 10
+$fift -I ${inc} -s send-vesting-wallet.fif ${base_file} ${dest_address} 268 ${seqno} ${amount}
